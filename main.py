4da2afdd7429e466d6c01b69cd93ba7590112269
Mari <testirovsik0005@mail.ru>
2025-03-29 16:34:35 +0300

    отредактировала маршруты

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ def read_root():
     return {"Hello": "Tutututu"}
 
 @app.get("/about_me")
-def show_about_me(a:int, b:int):
+def show_about_me():
 
     return{
         'name' : 'Mari',
         'group' : 'T-323901',
         'age' : '20',
-        's' : a + b
+        'E-mail' : 'testirovsik0005@mail.ru'
     }
 
-@app.get("/random_number")
+@app.get("/rnd")
 def show_random_number():
     return{'random_number' : randint(1,10)}
 
-@app.get("/s_triangle")
+@app.get("/t_square")
 def search_s(a:int = Query(gt=0), b:int = Query(gt=0), c:int = Query(gt=0)):
+    P = a + b + c
     p = (a + b + c)/2
     s = math.sqrt(p*(p-a)*(p-b)*(p-c))
     return{
-        'S' : s
+        'P=' : P,
+        'S=' : s
     }
